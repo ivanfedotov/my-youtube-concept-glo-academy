d606080c9a4c3785714c2e01506dbd54ff5e2bc0
Ivan Fedotov <ivanfedotovru@gmail.com>
2021-04-08 22:27:48 +0300

    Вертка дополнительных слайдеров (swiper.js)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.channel-slider', {
+const swiper1 = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
     slidesPerView: 6,
@@ -7,6 +7,32 @@ const swiper = new Swiper('.channel-slider', {
     navigation: {
       nextEl: '.channel-button-next',
       prevEl: '.channel-button-prev',
+    },
+  
+  });
+
+  const swiper2 = new Swiper('.slider-2', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-2-next',
+      prevEl: '.slider-2-prev',
+    },
+  
+  });
+
+  const swiper3 = new Swiper('.slider-3', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-3-next',
+      prevEl: '.slider-3-prev',
     },
   
   });
